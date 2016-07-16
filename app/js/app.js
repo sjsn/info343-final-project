a4d8ebffb6408f42fe72684b449c3ebe11622fde
Samuel San Nicolas <samuelsan3@gmail.com>
2016-07-16 15:13:04 -0700

    Add character getter

--- app/js/app.js
+++ app/js/app.js
@@ -44,7 +44,8 @@ app.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $ur
 
 }]);
 
-app.controller('HomeCtrl', ['$scope', '$firebaseAuth', '$firebaseArray', '$firebaseObject', function($scope, $firebaseAuth, $firebaseArray, $firebaseObject) {
+app.controller('HomeCtrl', ['$scope', '$firebaseAuth', '$firebaseArray', '$firebaseObject', "$http",
+	function($scope, $firebaseAuth, $firebaseArray, $firebaseObject, $http) {
 
 		var baseRef = firebase.database().ref();
 
@@ -101,14 +102,14 @@ app.controller('HomeCtrl', ['$scope', '$firebaseAuth', '$firebaseArray', '$fireb
 		// Create a firebaseObject of your users, and store this as part of $scope
 		$scope.users = $firebaseObject(usersRef);		
 
-		
-}]);
-
-app.controller("CardsCtrl", ["$scope", "CharService", function($scope, CharService) {
 
 }]);
 
-app.controller("DetailsCtrl", ["$scope", "CharService", function($scope, CharService) {
+app.controller("CardsCtrl", ["$scope", function($scope) {
+
+}]);
+
+app.controller("DetailsCtrl", ["$scope", function($scope) {
 
 }]);
 
@@ -117,34 +118,62 @@ app.controller("StoreCtrl", ["$scope", "CharService", function($scope, CharServi
 }]);
 
 // Controller for the game
-app.controller("GameCtrl", ["$scope", "UserService", "CharService", function($scope, UserService, CharService) {
+app.controller("GameCtrl", ["$scope", "UserService", "$http", function($scope, UserService, $http) {
 
+	// Index of current character in Marvel API
+	var charNum = Math.floor((Math.random() * 1500)) + 1;
+	var character;
+	// Gets character from Marvel API
+	var getChar = function() {
+		$http.get("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=fef7d5ab447d43d61cbb442f9c76073f&hash=0151cc0f29d81edd53d5bc5e4ee1122b"
+			+ "&limit=1&offset=" + charNum).success(function(results) {
+			if (results.data.results[0].thumbnail.path == "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available") {
+			charNum = Math.floor((Math.random() * 1500)) + 1;
+				getChar();
+			} else {
+				character = results.data.results[0];
+				console.log(character);
+			}
+		});
+	};
 
+	getChar();
 
 }]);
 
 // Controller for the leaderboard
-app.controller("LeaderboardsCtrl", ["$scope", "UserService", "CharService", function($scope, UserService, CharService) {
+app.controller("LeaderboardsCtrl", ["$scope", "UserService", function($scope, UserService) {
 
 
 
 }]);
 
-// A service to keep track of all possible characters in the game.
-// Loads on initial page load to reduce laggy loads / number of API calls while using app
-app.factory("CharService", ["$http", function($http) {
+// // A service to keep track of all possible characters in the game.
+// // Loads on initial page load to reduce laggy loads / number of API calls while using app
+// app.factory("CharService", ["$http", function($http) {
 
-	var service = {};
+// 	var service = {};
 
-	// Instantiates the characters array in the service 
-	$http.get("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=fef7d5ab447d43d61cbb442f9c76073f&hash=0151cc0f29d81edd53d5bc5e4ee1122b")
-	.success(function(results) {
-		// An array of character information. 
-		// Name is .name, description is .description, image is .thumbnail.path + "" + .thumbnail.extension
-		service.characters = results.data.results;
-	});
+// 	// Instantiates the characters array in the service 
+// 	service.characters = [];
+// 	for (var i = 0; i < 10; i++) {
+// 		$http.get("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=fef7d5ab447d43d61cbb442f9c76073f&hash=0151cc0f29d81edd53d5bc5e4ee1122b"
+// 			+ "&limit=100&offset=" + (i * 100))
+// 		.success(function(results) {
+// 			// An array of character information. 
+// 			// Name is .name, description is .description, image is .thumbnail.path + "." + .thumbnail.extension
+// 			_(results.data.results).forEach(function(character) {
+// 				// Only add characters with a thumbnail available
+// 				if (character.thumbnail.path != "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available") {
+// 					service.characters.push(character);
+// 				}
+// 			});
+// 			console.log(service.characters);
+// 		});
+// 	}
 
-	return service;
+
+// 	return service;
 
 
-}]);
+// }]);
